82e9ade41ea261f612956ca084fb7c8a476aa823
Vaibhav Ramamoorthy <vaibhavr@berkeley.edu>
2022-09-20 22:16:07 -0700

    card dealing logic

--- main.py
+++ main.py
@@ -1,22 +1,51 @@
+import random
 from player import Player
 
 class Main:
     def __init__(self, players):
         self.players = players
         self.deck = {
-            "person": set("Peacock", "Scarlet", "Plum", "Green", "Mustard", "White"),
-            "weapon": set("Candlestick", "Rope", "Pistol", "Knife"),
-            "room": set("Living Room", "Hall", "Dining Room", "Kitchen", "Spa")
+            "person": set(["Peacock", "Scarlet", "Plum", "Green", "Mustard", "White"]),
+            "weapon": set(["Candlestick", "Rope", "Pistol", "Knife"]),
+            "room": set(["Living Room", "Hall", "Dining Room", "Kitchen", "Spa"])
         }
         self.envelope = {
-            "person": set(),
-            "weapon": set(),
-            "room": set()
+            "person": None,
+            "weapon": None,
+            "room": None
         }
 
-    def game():
-        pass
+    def deal(self):
+        self.envelope["person"] = self.deck["person"].pop()
+        self.envelope["weapon"] = self.deck["weapon"].pop()
+        self.envelope["room"] = self.deck["room"].pop()
+        # print("Envelope: " + str(self.envelope))
+        n_cards = sum([len(v) for v in self.deck.values()])
+        player_index = 0
+        while n_cards > 0:
+            r = random.randint(0, n_cards - 1)
+            # deciding which card to deal
+            if r < len(self.deck["person"]):
+                self.players[player_index].deal("person", self.deck["person"].pop())
+            elif r < len(self.deck["person"]) + len(self.deck["weapon"]):
+                self.players[player_index].deal("weapon", self.deck["weapon"].pop())
+            else:
+                self.players[player_index].deal("room", self.deck["room"].pop())
+            # incrementing player to deal to
+            if player_index == len(self.players) - 1:
+                player_index = 0
+            else:
+                player_index += 1
+            n_cards -= 1
+        # printing cards for dealer confirmation
+        # for player in players:
+        #     print(player.name + str(player.cards))
 
-players = [Player("Vaibhav"), Player("Kaanchana")]
+    def game(self):
+        self.deal()
+        return(None)
+
+players = [Player("Vaibhav"), Player("Kaanchana"), Player("Sruthi"), Player("Rohith")]
 game = Main(players)
 winner = game.game()
+print(winner)
--- player.py
+++ player.py
@@ -2,9 +2,9 @@ class Player:
     def __init__(self, name):
         self.name = name
         self.candidates = {
-            "person": set("Peacock", "Scarlet", "Plum", "Green", "Mustard", "White"),
-            "weapon": set("Candlestick", "Rope", "Pistol", "Knife"),
-            "room": set("Living Room", "Hall", "Dining Room", "Kitchen", "Spa")
+            "person": set(["Peacock", "Scarlet", "Plum", "Green", "Mustard", "White"]),
+            "weapon": set(["Candlestick", "Rope", "Pistol", "Knife"]),
+            "room": set(["Living Room", "Hall", "Dining Room", "Kitchen", "Spa"])
         }
         self.cards = {
             "person": set(),
@@ -12,15 +12,15 @@ class Player:
             "room": set()
         }
 
-    @abstractmethod
+    def deal(self, type, card):
+        self.cards[type].add(card)
+
     def should_accuse(self):
         pass
 
-    @abstractmethod
     def accuse(self):
         pass
 
-    @abstractmethod
     def guess(self):
         pass
 
