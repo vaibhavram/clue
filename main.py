b8e2bc43837d0124ec760f262ab6ebd1e4881ffb
Vaibhav Ramamoorthy <vaibhavr@berkeley.edu>
2022-09-21 21:39:37 -0700

    random player done

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from player import Player
 class Main:
     def __init__(self, players):
         self.players = players
+        # need some notion of which players are active/inactive in the game
         self.deck = {
             "person": set(["Peacock", "Scarlet", "Plum", "Green", "Mustard", "White"]),
             "weapon": set(["Candlestick", "Rope", "Pistol", "Knife"]),
@@ -21,7 +22,7 @@ class Main:
         self.envelope["person"] = self.deck["person"].pop()
         self.envelope["weapon"] = self.deck["weapon"].pop()
         self.envelope["room"] = self.deck["room"].pop()
-        # print("Envelope: " + str(self.envelope))
+        print("Envelope: " + str(self.envelope))
         n_cards = sum([len(v) for v in self.deck.values()])
         player_index = 0
         while n_cards > 0:
@@ -42,15 +43,53 @@ class Main:
         # printing cards for dealer confirmation
         # for player in players:
         #     print(player.name + str(player.cards))
+        #     print(player.name + str(player.candidates))
 
     def check_envelope(self, person, weapon, room):
         return(self.envelope["person"] == person and self.envelope["weapon"] == weapon and self.envelope["room"] == room)
 
     def game(self):
         self.deal()
-        return(None)
+        n = 0
+        turn_index = 0
+        winner = None
+        while not winner:
+            print("--TURN " + str(n) + ": " + self.players[turn_index].get_name() + "--")
+            print(self.players[turn_index].candidates)
+            if self.players[turn_index].should_accuse():
+                if self.players[turn_index].accuse():
+                    winner = self.players[turn_index].get_name()
+                else:
+                    pass
+                    # eventually will need to enable other players to accuse as well, in order of turn is probably fairest
+            else:
+                type1, card1, type2, card2 = self.players[turn_index].guess()
+                print("GUESS: " + self.players[turn_index].get_name() + " guesses " + card1 + ", " + card2)
+                shown = False
+                for i in range(len(self.players) - 1):
+                    type, card = self.players[(turn_index + i + 1) % len(self.players)].show_card(type1, card1, type2, card2)
+                    if type and card:
+                        print("SHOW: " + self.players[(turn_index + i + 1) % len(self.players)].get_name() + " shows " + card)
+                        self.players[turn_index].process_shown_card(type, card)
+                        shown = True
+                        break
+                    else:
+                        print("SHOW: " + self.players[(turn_index + i + 1) % len(self.players)].get_name() + " does not show")
+                if not shown:
+                    self.players[turn_index].process_shown_card(None, None, type1, card1, type2, card2)
+            if turn_index == len(self.players) - 1:
+                turn_index = 0
+            else:
+                turn_index += 1
+            # avoid infinite loops
+            # if n > 30:
+            #     break
+            # else:
+            #     n += 1
+            n += 1
+        return(winner)
 
 players = [Player("Vaibhav"), Player("Kaanchana"), Player("Sruthi"), Player("Rohith")]
 game = Main(players)
 winner = game.game()
-print(winner)
+print("+++++ " + winner.upper() + " WINS +++++")
--- player.py
+++ player.py
@@ -1,10 +1,12 @@
+import random
+
 class Player:
     def __init__(self, name):
         self.name = name
         self.candidates = {
-            "person": set(["Peacock", "Scarlet", "Plum", "Green", "Mustard", "White"]),
-            "weapon": set(["Candlestick", "Rope", "Pistol", "Knife"]),
-            "room": set(["Living Room", "Hall", "Dining Room", "Kitchen", "Spa"])
+            "person": ["Peacock", "Scarlet", "Plum", "Green", "Mustard", "White"],
+            "weapon": ["Candlestick", "Rope", "Pistol", "Knife"],
+            "room": ["Living Room", "Hall", "Dining Room", "Kitchen", "Spa"]
         }
         self.cards = {
             "person": set(),
@@ -12,6 +14,9 @@ class Player:
             "room": set()
         }
         self.game = None
+
+    def get_name(self):
+        return(self.name)
 
     def assign_game(self, game):
         self.game = game
@@ -21,14 +26,47 @@ class Player:
         self.candidates[type].remove(card)
 
     def should_accuse(self):
-        return(all([len(v) for v in self.candidates.values() == 1]))
+        return(all([elem == 1 for elem in [len(v) for v in self.candidates.values()]]))
 
-    def accuse(self, person, weapon, room):
+    def accuse(self):
+        person = random.sample(self.candidates["person"], 1)[0]
+        weapon = random.sample(self.candidates["weapon"], 1)[0]
+        room = random.sample(self.candidates["room"], 1)[0]
         return(self.game.check_envelope(person, weapon, room))
 
     def guess(self):
-        # options = ["person", "weapon", "room"]
-        pass
+        options = ["person", "weapon", "room"]
+        type1, type2 = random.sample(options, 2)
+        card1 = random.sample(self.candidates[type1], 1)[0]
+        card2 = random.sample(self.candidates[type2], 1)[0]
+        return(type1, card1, type2, card2)
 
-    def has_card(self, type, card):
-        return(card in self.cards(type))
+    def show_card(self, type1, card1, type2, card2):
+        if card1 in self.cards[type1] and card2 in self.cards[type2]:
+            if random.randint(0,1) == 0:
+                return(type1, card1)
+            else:
+                return(type2, card2)
+        elif card1 in self.cards[type1]:
+            return(type1, card1)
+        elif card2 in self.cards[type2]:
+            return(type2, card2)
+        else:
+            return(None, None)
+
+    def process_shown_card(self, type, card, guesstype1 = None, guesscard1 = None, guesstype2 = None, guesscard2 = None):
+        if not type and not card:
+            if guesscard1 not in self.cards[guesstype1]:
+                self.candidates[guesstype1] = set([guesscard1])
+            if guesscard2 not in self.cards[guesstype2]:
+                self.candidates[guesstype2] = set([guesscard2])
+        elif card in self.candidates[type]:
+            self.candidates[type].remove(card)
+
+
+
+
+
+
+
+
